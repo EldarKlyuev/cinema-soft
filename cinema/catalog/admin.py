ae4990c9572dc91c1f7c3dd36b2538566a0f501d
Eldar Klyuev <e.s.klyuev@gmail.com>
2023-03-27 09:36:19 +0300

    Add models

--- cinema/catalog/admin.py
+++ cinema/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from catalog.models import User, Cinema
+
+admin.site.register(User)
+admin.site.register(Cinema)
 
 # Register your models here.
--- cinema/catalog/models.py
+++ cinema/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(blank=False, max_length=20)
+    sur_name = models.CharField(blank=False, max_length=20)
+    second_name = models.CharField(blank=False, max_length=20)
+    age = models.IntegerField(blank=False, max_length=4)
+    gender = models.CharField(blank=False, max_length=20)
+    
+
+    
+
+class Cinema(models.Model):
+    name = models.CharField(blank=False, max_length=20)
+    gender_rate = models.IntegerField(blank=False, max_length=4)
